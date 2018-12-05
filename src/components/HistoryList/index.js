2a9473a19004b5e27c63926817004e231a4b59d2
n1re <mgfile08@gmail.com>
2018-12-05 20:26:33 +0500

    HistoryList is class component

--- src/components/HistoryList/index.js
+++ src/components/HistoryList/index.js
@@ -1,43 +1,45 @@
 import React from 'react';
 import './style.css';
 
-function HistoryList({steps, jumpTo}) {
-
-  const formPosition = ({column, row}) => {
-    if (column === 0) return;
-
-    return (
-      <span>{`Column: ${column}, Row: ${row}`}<br></br></span>
-    );
-  }
-
-  const formButton = (index) => {
-    const step = ++index;
-    const innerText = `Go to step`;
-
-    return (
-      <button onClick={() => jumpTo(step)}>
-        {innerText}
-      </button>
-    );
-  }
-
-  const realSteps = steps.slice(1, ++steps.length);
+class HistoryList extends React.Component {
+  render() {
+    const formPosition = ({column, row}) => {
+      if (column === 0) return;
   
-  const listItems = realSteps.map(({ position }, index) => {
+      return (
+        <span>{`Column: ${column}, Row: ${row}`}<br></br></span>
+      );
+    }
+  
+    const formButton = (index) => {
+      const step = ++index;
+      const innerText = `Go to step`;
+  
+      return (
+        <button onClick={() => this.props.jumpTo(step)}>
+          {innerText}
+        </button>
+      );
+    }
+    
+    const steps = this.props.steps;
+    const realSteps = steps.slice(1, ++steps.length);
+    
+    const listItems = realSteps.map(({ position }, index) => {
+      return (
+        <li key={index}>
+          {formPosition(position)}
+          {formButton(index)}
+        </li>
+      );
+    });
+  
     return (
-      <li key={index}>
-        {formPosition(position)}
-        {formButton(index)}
-      </li>
+      <ol>
+        {listItems}
+      </ol>
     );
-  });
-
-  return (
-    <ol>
-      {listItems}
-    </ol>
-  );
+  }
 }
 
 export default HistoryList;
